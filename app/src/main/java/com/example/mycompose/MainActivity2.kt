0a888990243086b7af03bf9d00715378d496f13c
leeranzhi <liguoqing.me@gmail.com>
2021-05-22 20:33:39 +0800

    增加列表文字选中及取消动画.

--- app/src/main/java/com/example/mycompose/MainActivity2.kt
+++ app/src/main/java/com/example/mycompose/MainActivity2.kt
@@ -3,12 +3,17 @@ package com.example.mycompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -43,9 +48,16 @@ fun MyApp(content: @Composable () -> Unit) {
 
 @Composable
 fun Greeting2(name: String) {
+    var isSelected by remember {
+        mutableStateOf(false)
+    }
+    val backgroundColor by animateColorAsState(if (isSelected) Color.Red else Color.Transparent)
     Text(
         text = "Hello $name!",
-        modifier = Modifier.padding(24.dp)
+        modifier = Modifier
+            .padding(24.dp)
+            .background(backgroundColor)
+            .clickable(onClick = { isSelected = !isSelected })
     )
 }
 

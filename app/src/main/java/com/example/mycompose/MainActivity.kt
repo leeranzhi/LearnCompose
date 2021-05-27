e12dd51737ba0e2d13b6ad4ef32bc5ec6f09bd03
leeranzhi <liguoqing.me@gmail.com>
2021-05-27 18:03:43 +0800

    增加文字和图片居中属性.

--- app/src/main/java/com/example/mycompose/MainActivity.kt
+++ app/src/main/java/com/example/mycompose/MainActivity.kt
@@ -13,10 +13,12 @@ import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material.*
 import androidx.compose.material.MaterialTheme.typography
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -60,14 +62,16 @@ fun JetpackCompose() {
                 modifier = Modifier
                     .height(180.dp)
                     .width(180.dp)
-                    .clip(shape = RoundedCornerShape(8.dp)),
+                    .clip(shape = RoundedCornerShape(8.dp))
+                    .align(Alignment.CenterHorizontally),
                 contentScale = ContentScale.Crop,
 
                 )
             AnimatedVisibility(visible = expanded) {
                 Text(
                     text = "Jetpack Compose",
-                    style = MaterialTheme.typography.h2
+                    style = MaterialTheme.typography.h2,
+                    textAlign = TextAlign.Center
                 )
             }
         }

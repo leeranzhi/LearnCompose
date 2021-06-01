8c8722be97e5a99f005eaf151aff5decde6e5042
leeranzhi <liguoqing.me@gmail.com>
2021-06-01 16:25:18 +0800

    增加ScrollList实例.

--- LayoutsCodelab/app/src/main/java/com/example/layoutscodelab/Lists.kt
+++ LayoutsCodelab/app/src/main/java/com/example/layoutscodelab/Lists.kt
@@ -6,14 +6,17 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.coil.rememberCoilPainter
+import kotlinx.coroutines.launch
 
 //Compose它也提供了仅构成和布置当前可见项的惰性列表
 @Composable
@@ -83,4 +86,44 @@ fun ImageListItem(index: Int) {
 @Composable
 fun ImageListItemPreView() {
     ImageListItem(1)
+}
+
+@Composable
+fun ScrollingList() {
+    val listSize = 100
+// We save the scrolling position with this state
+    val scrollState = rememberLazyListState()
+// We save the coroutine scope where our animated scroll will be executed
+    val coroutineScope = rememberCoroutineScope()
+
+    Column {
+        Row {
+            Button(onClick = {
+                coroutineScope.launch {
+                    scrollState.animateScrollToItem(0)
+                }
+            }) {
+                Text(text = "Scroll to top")
+            }
+
+            Button(onClick = {
+                coroutineScope.launch {
+                    scrollState.animateScrollToItem(listSize - 1)
+                }
+            }) {
+                Text(text = "Scroll to end")
+            }
+        }
+        LazyColumn(state = scrollState) {
+            items(listSize) {
+                ImageListItem(index = it)
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ScrollingListPreView(){
+    ScrollingList()
 }
